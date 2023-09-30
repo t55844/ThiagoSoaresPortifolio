9f0137760ee36129007b86c06442e735ea8984da
Thiago Soares <thiagosoares111@yahoo.com.br>
2023-09-30 17:52:54 -0300

    version 1.0 done start deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Abilities from "./components/Abilities";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Header from "./components/Header";
 import MyProjects from "./components/MyProjects";
 
 function App() {
--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -5,13 +5,13 @@ export default function MyProjects() {
     <section className="work py-16" id="work">
       <div className="container mx-auto">
         <h2 className="text-3xl font-semibold text-center mb-6 border-b-8 border-gray-300 bg-gray-200">
-          portfólio
+          projeto
         </h2>
         <h3 className="text-2xl font-semibold text-center mb-4">
-          uma seleção do que construí
+          Um projeto de exemplo que eu fiz e que esta ativo na internet
         </h3>
         {/* Substitua pelos itens do seu portfólio */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="">
           <CardMyProject
             title="Plants Lovers"
             description="Introdução Plants Lovers é uma plataforma baseada na web projetada para que os amantes de plantas explorem, aprendam e acompanhem suas plantas favoritas. É o lugar perfeito para se conectar com outros entusiastas de plantas, descobrir novas espécies e construir sua própria coleção digital de plantas."
